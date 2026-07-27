fd250331369f868402cb588f24f7375ecbedf6f2
sulejmonn047-dotcom <sulejmonn047@gmail.com>
2026-07-27 19:33:52 +0500

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -14,19 +14,19 @@ async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text
 
     if text == "📚 Китобҳо":
-        await update.message.reply_text(
-            "Рӯйхати китобҳо:\n"
-            "1. Кӯдаки нобиғаи ман\n"
-            "2. Китоби 2\n"
-            "3. Китоби 3"
-        )
+    book_list = "📚 Рӯйхати китобҳо:\n\n"
+
+    for book in BOOKS:
+        book_list += f"{book['id']}. {book['name']}\n"
+
+    await update.message.reply_text(book_list)
 
     elif text == "💰 Нархнома":
         await update.message.reply_text(
             "1 китоб — 10 сомонӣ\n"
             "2 китоб — 15 сомонӣ\n"
             "6 китоби Саидмурод Давлатов — 45 сомонӣ\n"
-            "Ҳамаи 24 китоб — 180 сомонӣ"
+            "Ҳамаи 24 китоб —? сомонӣ"
         )
 
     elif text == "💳 Пардохт":
