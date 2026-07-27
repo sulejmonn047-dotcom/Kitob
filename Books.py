72d911430c46f111cc48a8b14f66e0076b6d2a55
sulejmonn047-dotcom <sulejmonn047@gmail.com>
2026-07-27 14:02:06 +0500

    Update Books.py

--- Books.py
+++ Books.py
@@ -1,7 +1,1 @@
-# books.py
 
-BOOKS = [
-    {"id": 1, "name": "Кӯдаки нобиғаи ман", "price": 10, "file": "books/book1.pdf"},
-    {"id": 2, "name": "Китоби 2", "price": 10, "file": "books/book2.pdf"},
-    {"id": 3, "name": "Китоби 3", "price": 10, "file": "books/book3.pdf"},
-]
