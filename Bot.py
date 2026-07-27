1cae107a8de542b3e76bef5e498ccaaf2f836c3b
sulejmonn047-dotcom <sulejmonn047@gmail.com>
2026-07-27 19:08:20 +0500

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from threading import Thread
+
 from telegram.ext import (
     Application,
     CommandHandler,
@@ -9,30 +10,41 @@ from telegram.ext import (
 
 from config import BOT_TOKEN
 from handlers import start, message_handler
+
+
 web_app = Flask(__name__)
+
 
 @web_app.route("/")
 def home():
     return "Bot is running"
 
+
 def run_server():
     web_app.run(host="0.0.0.0", port=10000)
 
+
 def keep_alive():
     Thread(target=run_server).start()
+
+
 def main():
+    # Барои Render порт мекушояд
+    keep_alive()
+
     app = Application.builder().token(BOT_TOKEN).build()
 
     # Командаи /start
     app.add_handler(CommandHandler("start", start))
 
-    # Коркарди паёмҳои матнӣ
+    # Паёмҳои матнӣ
     app.add_handler(
         MessageHandler(filters.TEXT & ~filters.COMMAND, message_handler)
     )
 
     print("Бот фаъол шуд...")
     app.run_polling()
+
 
 if __name__ == "__main__":
     main()
