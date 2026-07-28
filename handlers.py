f5b4e1fea1cba99373e9bb7c93fe65767d5893eb
sulejmonn047-dotcom <sulejmonn047@gmail.com>
2026-07-28 08:40:59 +0500

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -4,14 +4,11 @@ from telegram import Update
 from telegram.ext import ContextTypes
 from keyboards import main_keyboard
 
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "📚 Ба боти фурӯши китобҳо хуш омадед!",
         reply_markup=main_keyboard
     )
-
-
 async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text
 
