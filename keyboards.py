126360bc7ccd5601ce596f879243813389e39286
sulejmonn047-dotcom <sulejmonn047@gmail.com>
2026-07-28 08:42:35 +0500

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -2,11 +2,19 @@
 from telegram import ReplyKeyboardMarkup, KeyboardButton
 
 main_keyboard = ReplyKeyboardMarkup(
-    [
-        [KeyboardButton("📚 Китобҳо")],
-        [KeyboardButton("💰 Нархнома")],
-        [KeyboardButton("💳 Пардохт")],
-        [KeyboardButton("📞 Тамос бо админ")]
+    keyboard=[
+        [
+            KeyboardButton("📚 Китобҳо")
+        ],
+        [
+            KeyboardButton("💰 Нархнома")
+        ],
+        [
+            KeyboardButton("💳 Пардохт")
+        ],
+        [
+            KeyboardButton("📞 Тамос бо админ")
+        ]
     ],
     resize_keyboard=True
 )
